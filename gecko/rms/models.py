26d1e203f408602347ef411c33c4b747e893f855
pradam <pradamabhilash@gmail.com>
2018-12-15 13:20:44 +0530

    add abstract class

--- gecko/rms/models.py
+++ gecko/rms/models.py
@@ -1,3 +1,23 @@
 from django.contrib.postgres.fields import JSONField
 from django.db import models
-# Create your models here.
+
+OPTIONAL = {'blank': True, 'null': True}
+
+ACTIVE_CHOICES = ((0, 'Inactive'), (2, 'Active'),)
+
+
+class BaseContent(models.Model):
+    active = models.PositiveIntegerField(choices=ACTIVE_CHOICES,
+                                         default=2)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def switch(self):
+        self.active = {2: 0, 0: 2}[self.active]
+        self.save()
+
+    def __str__(self):
+        return self.name
