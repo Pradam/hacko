6c2d7d6c8e9f07263add372586c505fd563ffab2
kausik15 <kausik15@outlook.com>
2018-12-15 15:24:06 +0530

    add migration files and db files for acko

--- gecko/rms/models.py
+++ gecko/rms/models.py
@@ -21,3 +21,53 @@ class BaseContent(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class HistoryData(BaseContent):
+    age = models.PositiveIntegerField()
+    gender = models.TextField(editable=False)
+    name = models.CharField(max_length=255, unique=True)
+
+
+class HistoryData(BaseContent):
+
+    ALCOHOL_CHOICES = (
+        (1, 'NO'),
+        (2, 'OCCATIONALLY'),
+        (3, 'HEAVY')
+    )
+
+    SMOKING_CHOICES = (
+        (1, 'NO'),
+        (2, 'MODERATE'),
+        (3, 'CHAIN')
+    )
+
+
+    CLAIM_CHOICES = (
+        (1, 'NO CLAIM'),
+        (2, 'REJECTED'),
+        (3, 'ACCEPTED')
+    )
+
+    GENDER_CHOICES = (
+        (1, 'MALE'),
+        (2, 'FEMALE'),
+        (3, 'TRANSGENDER')
+    )
+
+    aadhar_id = models.IntegerField()
+    name = models.TextField()
+    gender = models.IntegerField(choices=GENDER_CHOICES)
+    age = models.PositiveIntegerField()
+    premium = models.PositiveIntegerField()
+    coverage = models.PositiveIntegerField()
+    is_alcoholic = models.IntegerField(choices=ALCOHOL_CHOICES, default=1)
+    is_smoker = models.IntegerField(choices=SMOKING_CHOICES, default=1)
+    claimed = models.IntegerField(choices=CLAIM_CHOICES, default=1)
+
+    class Meta:
+        unique_together = ('aadhar_id',)
+
+    def __str__(self):
+        return self.aadhar_id
